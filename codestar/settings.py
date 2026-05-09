5d42dbd5098ed8cd2c404fe359094a4430cdfb02
louisae452 <louisae452@msn.com>
2026-05-09 09:52:21 +0100

    Add fields to model Post

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-^fl*&fs-75&!w&cvq9j8qv$e(bouc6e_pbaz@%t9e(^8nk@bkr'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
