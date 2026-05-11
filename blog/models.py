554a9be91d013074b188d873b63f29ca61af0411
louisae452 <louisae452@msn.com>
2026-05-11 09:23:28 +0100

    Add home view

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True) 
+    
     # Add metadata
     class Meta:
         ordering = ["-created_on"]
@@ -24,6 +25,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
