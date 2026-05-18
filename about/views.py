a3f654496b4d090132246c2c2bf0ee914c83b936
louisae452 <louisae452@msn.com>
2026-05-18 13:05:00 +0100

    Add views, template and urls to About app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import About
+
+
+def about_me(request):
+    """
+    Renders the About page
+    """
+    toshow = About.objects.order_by("updated_on").first()
+    return render(
+        request,
+        "about/about.html",
+        {"about": toshow},
+    )
+    
+
+
 
 # Create your views here.
--- codestar/urls.py
+++ codestar/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     # path('blog', blog_views.blog, name = 'blog'),
 
     path('admin/', admin.site.urls),
-    
+      
+    path("about/", include("about.urls"), name="about-urls"),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
 ]
