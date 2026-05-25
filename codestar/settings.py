3e3e482386f5f0e9b2d971be943382ea98868b98
louisae452 <louisae452@msn.com>
2026-05-25 09:34:24 +0100

    Enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -52,7 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
